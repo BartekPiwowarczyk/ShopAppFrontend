2da45cda1c8fa888397ccf990a52f1ba9be4c37c
Bartek <bartekpiw1995@gmail.com>
2023-07-14 17:48:36 +0200

    Refactoring i test placeOrder

--- src/app/modules/order/order.component.ts
+++ src/app/modules/order/order.component.ts
@@ -18,6 +18,7 @@ export class OrderComponent implements OnInit{
   formGroup!: FormGroup;
   orderSummary!: OrderSummary;
   initData!: InitData;
+  errorMessage = false;
 
   private statuses = new Map<string,string>([
     ["NEW","Nowe"]
@@ -66,9 +67,13 @@ export class OrderComponent implements OnInit{
         shipmentId: Number(this.formGroup.get('shipment')?.value.id),
         paymentId: Number(this.formGroup.get('payment')?.value.id)
       } as OrderDto)
-      .subscribe(orderSummary => {
+      .subscribe({
+        next: orderSummary => {
         this.orderSummary = orderSummary;
-       this.cookieService.delete("cartId"); 
+       this.cookieService.delete("cartId");
+       this.errorMessage = false;
+        },
+       error: err => this.errorMessage = true
       })
     }
   }
