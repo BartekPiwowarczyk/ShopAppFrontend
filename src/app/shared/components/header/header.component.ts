27ca419e8581d4023ef13d690a40d3336c821b0e
bpiw <bartekpiw1995@gmail.com>
2023-08-23 14:47:16 +0200

    Refactoring

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { HeaderService } from './header.service';
 import { CartIconService } from 'src/app/modules/common/service/cart-icon.service';
+import { JwtService } from 'src/app/modules/common/service/jwt.service';
 
 @Component({
   selector: 'app-header',
@@ -12,17 +13,20 @@ export class HeaderComponent implements OnInit{
 
   title="Shop";
   cartProductCounter ="";
+  isLoggedIn = false;
 
   constructor(
     private cookieService: CookieService,
     private headerService: HeaderService,
-    private cartIconService: CartIconService
+    private cartIconService: CartIconService,
+    private jwtService: JwtService
     ) {}
 
   ngOnInit(): void {
     this.getCountProducts(); 
     this.cartIconService.subject
       .subscribe(counter => this.cartProductCounter = String(counter ? counter:""));
+      this.isLoggedIn = this.jwtService.isLoggedIn();
   }
 
   getCountProducts() {
