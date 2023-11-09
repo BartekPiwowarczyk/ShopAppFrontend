130eb1ff026743b37dbbd53f1fcef0c53cbeab49
bpiw <bartekpiw1995@gmail.com>
2023-11-09 23:12:35 +0100

    Home page

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomePageDto } from './model/homePageDto';
+import { HomeService } from './home.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  homePageData!: HomePageDto;
+
+  constructor(private homeService: HomeService) {}
+
+  ngOnInit(): void {
+      this.getHomePageData();
+  }
+
+  getHomePageData() {
+      this.homeService.getHomePageData()
+      .subscribe(homePageData => this.homePageData = homePageData);
+  }
 }
